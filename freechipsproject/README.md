971229af4ebb973fee79c8f8ace1301d624ae000
KRITIK BHIMANI <bhimanikritik@gmail.com>
2018-02-25 14:57:08 +0530

    add rocket-chip as submodule and remove freechipsproject directory

--- freechipsproject/README.md
+++
@@ -1,1 +0,0 @@
-Source files in this directory have been reused from https://github.com/freechipsproject/rocket-chip and https://github.com/ucb-bar/berkeley-hardfloat 
--- project/build.scala
+++ project/build.scala
@@ -11,6 +11,11 @@ object BuildSettings extends Build {
     "chisel-iotesters" -> "latest.release"//"1.1-SNAPSHOT"
     )
 
+  val rocketSettings = Seq (
+    libraryDependencies ++= Seq("org.json4s" %% "json4s-jackson" % "3.5.0"),
+    addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+  )
+
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     version      := buildVersion,
@@ -28,70 +33,29 @@ object BuildSettings extends Build {
 
   lazy val common = Project("common", file("common"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/common"))
-  lazy val rv32_1stage = Project("rv32_1stage", file("rv32_1stage"), settings = buildSettings ++ chipSettings
+  lazy val rv32_1stage = Project("rv32_1stage", file("rv32_1stage"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/rv32_1stage")) dependsOn(common)
-  lazy val rv32_2stage = Project("rv32_2stage", file("rv32_2stage"), settings = buildSettings ++ chipSettings
+  lazy val rv32_2stage = Project("rv32_2stage", file("rv32_2stage"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/rv32_2stage")) dependsOn(common)
-  lazy val rv32_3stage = Project("rv32_3stage", file("rv32_3stage"), settings = buildSettings ++ chipSettings
+  lazy val rv32_3stage = Project("rv32_3stage", file("rv32_3stage"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/rv32_3stage")) dependsOn(common)
-  lazy val rv32_5stage = Project("rv32_5stage", file("rv32_5stage"), settings = buildSettings ++ chipSettings
+  lazy val rv32_5stage = Project("rv32_5stage", file("rv32_5stage"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/rv32_5stage")) dependsOn(common)
-  lazy val rv32_ucode  = Project("rv32_ucode", file("rv32_ucode"), settings = buildSettings ++ chipSettings
+  lazy val rv32_ucode  = Project("rv32_ucode", file("rv32_ucode"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/rv32_ucode")) dependsOn(common)
-  lazy val fpgazynq  = Project("fpgazynq", file("fpgazynq"), settings = buildSettings ++ chipSettings
+  lazy val fpgazynq  = Project("fpgazynq", file("fpgazynq"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/fpgazynq")
-    ++Seq(resourceDirectory in Compile := baseDirectory.value / "../vsrc")) dependsOn(common,rv32_3stage,rocketchip)
-  lazy val fpgaartix  = Project("fpgaartix", file("fpgaartix"), settings = buildSettings ++ chipSettings
+    ++Seq(resourceDirectory in Compile := baseDirectory.value / "../vsrc")) dependsOn(common,rv32_3stage,chiprocket)
+  lazy val fpgaartix  = Project("fpgaartix", file("fpgaartix"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/fpgaartix")
-    ++Seq(resourceDirectory in Compile := baseDirectory.value / "../vsrc")) dependsOn(common,rv32_3stage,rocketchip,fpgazynq)
-  lazy val zynqsimtop  = Project("zynqsimtop", file("zynqsimtop"), settings = buildSettings ++ chipSettings
+    ++Seq(resourceDirectory in Compile := baseDirectory.value / "../vsrc")) dependsOn(common,rv32_3stage,chiprocket,fpgazynq)
+  lazy val zynqsimtop  = Project("zynqsimtop", file("zynqsimtop"), settings = buildSettings
     ++Seq(scalaSource in Compile := baseDirectory.value / "../src/zynqsimtop")) dependsOn(fpgazynq)
-  lazy val macros  = Project("macros", file("macros"), settings = buildSettings ++ chipSettings
-    ++Seq(scalaSource in Compile := baseDirectory.value / "../freechipsproject/macros"))
-  lazy val hardfloat  = Project("hardfloat", file("hardfloat"), settings = buildSettings ++ chipSettings
-    ++Seq(scalaSource in Compile := baseDirectory.value / "../freechipsproject/hardfloat"))
-  lazy val rocketchip  = Project("rocketchip", file("rocketchip"), settings = buildSettings ++ chipSettings
-    ++Seq(scalaSource in Compile := baseDirectory.value / "../freechipsproject/rocket-chip")) dependsOn(macros,hardfloat,common)
-
-  val elaborateTask = InputKey[Unit]("elaborate", "convert chisel components into backend source code")
-  val makeTask = InputKey[Unit]("make", "trigger backend-specific makefile command")
-
-  def runChisel(args: Seq[String], cp: Classpath, pr: ResolvedProject) = {
-     val numArgs = 1
-     require(args.length >= numArgs, "syntax: elaborate <component> [chisel args]")
-     val projectName = pr.id
-//     val packageName = projectName //TODO: valid convention?
-     val packageName = "Sodor" //TODO: celio change
-     val componentName = args(0)
-     val classLoader = new java.net.URLClassLoader(cp.map(_.data.toURL).toArray, cp.getClass.getClassLoader)
-     val chiselMainClass = classLoader.loadClass("Chisel.chiselMain$")
-     val chiselMainObject = chiselMainClass.getDeclaredFields.head.get(null)
-     val chiselMain = chiselMainClass.getMethod("run", classOf[Array[String]], classOf[Function0[_]])
-     val chiselArgs = args.drop(numArgs)
-     val component = classLoader.loadClass(packageName+"."+componentName)
-     val generator = () => component.newInstance()
-     chiselMain.invoke(chiselMainObject, Array(chiselArgs.toArray, generator):_*)
-  }
-
-  val chipSettings = Seq(
-    elaborateTask <<= inputTask { (argTask: TaskKey[Seq[String]]) =>
-      (argTask, fullClasspath in Runtime, thisProject) map {
-        runChisel
-      }
-    },
-    makeTask <<= inputTask { (argTask: TaskKey[Seq[String]]) =>
-      (argTask, fullClasspath in Runtime, thisProject) map {
-        (args: Seq[String], cp: Classpath, pr: ResolvedProject) => {
-          require(args.length >= 2, "syntax: <dir> <target>")
-          runChisel(args.drop(2), cp, pr)
-          val makeDir = args(0)
-          val target = args(1)
-          val jobs = java.lang.Runtime.getRuntime.availableProcessors
-          val make = "make -C" + makeDir + " -j" + jobs + " " + target
-          make!
-        }
-      }
-    }
-  )
+  lazy val macros  = Project("macros", file("macros"), settings = buildSettings
+    ++Seq(scalaSource in Compile := baseDirectory.value / "../rocket-chip/macros/src/main/scala"))
+  lazy val hardfloat  = Project("hardfloat", file("hardfloat"), settings = buildSettings
+    ++Seq(scalaSource in Compile := baseDirectory.value / "../rocket-chip/hardfloat/src/main/scala"))
+  lazy val chiprocket  = Project("chiprocket", file("chiprocket"), settings = buildSettings ++ rocketSettings
+    ++Seq(scalaSource in Compile := baseDirectory.value / "../rocket-chip/src/main/scala")) dependsOn(macros,hardfloat,common)
 }
 
